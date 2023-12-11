11480875202ddf3dc742a7fe8b129d2ea0f20aab
Yosef Ahmed <129369186+YA195@users.noreply.github.com>
2023-12-11 08:58:11 +0200

    Finished the Front end of the Login\signup

---
+++ WebApplication3/wwwroot/js/Custom.js
@@ -0,0 +1,16 @@
+﻿const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+document.addEventListener('DOMContentLoaded', () => {
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+
+    registerLink.addEventListener('click', () => {
+        wrapper.classList.add('active');
+    });
+
+    loginLink.addEventListener('click', () => {
+        wrapper.classList.remove('active');
+    });
+});

3a3b4becbbf6d3384c818e8fa88eac676b16364f
Yosef Ahmed <129369186+YA195@users.noreply.github.com>
2023-12-17 18:15:36 +0200

    Finished 60% of the GUI

---
+++ WebApplication3/Pages/Discount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication3.Pages
+{
+    public class DiscountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

cd662ee26cd4f1a2a5983c60c6b7745a88f37994
Yosef Ahmed <129369186+YA195@users.noreply.github.com>
2024-01-06 07:39:07 +0200

    finishid the front end of the items page

--- WebApplication3/Pages/Items.cshtml.cs
+++ WebApplication3/Pages/Items.cshtml.cs
@@ -1,7 +1,9 @@
-using System;
-using System.Collections.Generic;
+// ItemsModel.cs
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using WebApplication3.Models;
 
@@ -17,23 +19,47 @@ namespace WebApplication3.Pages
             _connectionString = configuration.GetConnectionString("DefaultConnection");
             _db = new DB(_connectionString);
         }
-        public List<MenuItem> MenuItems { get; set; }
+
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+        public int NewItemId { get; set; }
+
+        [BindProperty]
+        public MenuItem NewMenuItem { get; set; }
+
+        [BindProperty]
+        public Item ItemIdToDelete { get; set; }
+
+        [BindProperty]
+        public MenuItem UpdatedMenuItem { get; set; }
 
         public void OnGet()
         {
-            MenuItems = GetMenuItems();
-        }
-        // MenuItem.cs
-        public class MenuItem
-        {
-            public int ItemID { get; set; }
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public decimal Price { get; set; }
-            public string ImagePath { get; set; }
-            public string Category { get; set; }
+            MenuItems = GetMenuItems() ?? new List<MenuItem>();
+            NewItemId = GetNextId();
         }
 
+        public int GetNextId()
+        {
+            int nextId = 1;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT MAX(ItemID) FROM MenuItem";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    var result = command.ExecuteScalar();
+
+                    if (result != null && result != DBNull.Value)
+                    {
+                        nextId = Convert.ToInt32(result) + 1;
+                    }
+                }
+            }
+
+            return nextId;
+        }
 
         private List<MenuItem> GetMenuItems()
         {
@@ -66,5 +92,77 @@ namespace WebApplication3.Pages
 
             return menuItems;
         }
+
+        public IActionResult OnPost()
+        {
+            switch (Request.Form["handler"])
+            {
+                case "delete":
+                    return DeleteMenuItem(ItemIdToDelete);
+
+                default:
+                    return RedirectToPage();
+            }
+        }
+
+
+        public IActionResult DeleteMenuItem(Item item)
+        {
+            string resultMessage;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = $"DELETE FROM MenuItem WHERE ItemID = {item.itemid}";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    resultMessage = rowsAffected > 0
+                        ? "Item deleted successfully."
+                        : "Item not found or deletion failed.";
+                }
+            }
+
+            TempData["ResultMessage"] = resultMessage;
+            return RedirectToPage();
+        }
+
+
+
+
+
+        // MenuItem.cs
+        public class MenuItem
+        {
+            public int ItemID { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+            public string ImagePath { get; set; }
+            public string Category { get; set; }
+        }
+        public Item item { get; set; }
+        public class Item
+        {
+            [BindProperty]
+            public string itemname { get; set; }
+            [BindProperty]
+            public int itemid { get; set; }
+
+            [BindProperty]
+            public string description { get; set; }
+            [BindProperty]
+            public string imagepath { get; set; }
+            [BindProperty]
+            public string category { get; set; }
+            [BindProperty]
+            public decimal price { get; set; }
+
+
+        }
+
     }
 }
